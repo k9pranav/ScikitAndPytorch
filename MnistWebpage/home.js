5e3fadc4edc3eeed9dd9615cc63b033b4301de7d
k9pranav <k9.pranav@gmail.com>
2023-08-23 03:00:35 +0400

    Made an array the displays 1 co responding to the colored pixel/grid item

--- MnistWebpage/home.js
+++ MnistWebpage/home.js
@@ -3,6 +3,10 @@ let gridMain = document.querySelector('#grid');
 
 let isClicking = false;
 
+//The 28*28 pixel array
+let imageArray = Array(784).fill(0)
+
+
 // The grid setup
 gridMain.style.gridTemplateColumns = 'repeat(28, 1fr)';
 gridMain.style.gridTemplateRows = 'repeat(28, 1fr)';
@@ -21,18 +25,28 @@ gridMain.addEventListener('mouseup', () => {
 
 for (i = 0; i < 28*28; i++){
     let square = document.createElement('div');
-
-    square.addEventListener('mouseover', () => {
-        if (isClicking == true){
-            square.style.backgroundColor = 'black';
-        }
-    })
-
-    square.classList.add('boxBlank');
+    square.classList = i;
+    square.style.backgroundColor = 'white';
+    square.style.border = '5px solid';
+    square.style.borderWidth = '0.4px';
+    square.style.height = '15px';
+    square.style.width = '15px';
+    square.addEventListener('mouseover', squareFunc)
     gridMain.appendChild(square);
 }
 
 
+function squareFunc() {
+    if (isClicking == true){
+        this.style.backgroundColor = 'black';
+        imageArray[parseInt(this.className)] = 1;
+    }
+}
+
 
 //I can make a array list using query select all
+
+
+
+
 
