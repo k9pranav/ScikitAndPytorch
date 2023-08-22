479ff0296012672f7a285c7032af89d592818336
k9pranav <k9.pranav@gmail.com>
2023-08-22 20:08:02 +0400

    sorted the grid drawing. Will be looking how to incorporate canvas

--- MnistWebpage/home.js
+++ MnistWebpage/home.js
@@ -1,17 +1,38 @@
+
 let gridMain = document.querySelector('#grid');
 
-//The grid setup
-gridMain.style.gridTemplateColumns = 'repeat(28, 1fr)'
-gridMain.style.gridTemplateRows = 'repeat(28, 1fr)'
+let isClicking = false;
 
-//Adding the divs
+// The grid setup
+gridMain.style.gridTemplateColumns = 'repeat(28, 1fr)';
+gridMain.style.gridTemplateRows = 'repeat(28, 1fr)';
+
+
+
+gridMain.addEventListener('mousedown', () => {
+    isClicking = true;
+})
+
+gridMain.addEventListener('mouseup', () => {
+    isClicking = false;
+})
+
+// Adding the divs
+
 for (i = 0; i < 28*28; i++){
     let square = document.createElement('div');
-    square.style.backgroundColor = 'blue';
-    square.style.height = '10px';
-    square.style.width = '10px';
-    gridMain.appendChild(square)
+
+    square.addEventListener('mouseover', () => {
+        if (isClicking == true){
+            square.style.backgroundColor = 'black';
+        }
+    })
+
+    square.classList.add('boxBlank');
+    gridMain.appendChild(square);
 }
+
+
 
 //I can make a array list using query select all
 
